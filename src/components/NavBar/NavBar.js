fe3b5599a8df6fbe5d097a5f902241ed9792ca56
Raquel.Magnani <quel.magnani88@gmail.com>
2020-07-20 22:02:20 -0300

    fix route

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./navBar.css";
+import { Link } from "react-router-dom";
 import {
   Collapse,
   Navbar,
@@ -15,7 +16,6 @@ const NavBar = (props) => {
 
   const toggle = () => setIsOpen(!isOpen);
 
-  
   const { links } = props;
   return (
     <Navbar light expand="md">
@@ -24,9 +24,15 @@ const NavBar = (props) => {
       <Collapse isOpen={isOpen} navbar>
         <Nav className="ml-auto" navbar>
           {links.map((link) => (
-            
             <NavItem>
-              <NavLink href={link.path}>{link.description}</NavLink>
+              {link.path === "#descubra" ||
+              link.path === "#sobre" ? (
+                <NavLink href={link.path}>{link.description}</NavLink>
+              ) : (
+                <Link className="nav-link" to={link.path}>
+                  {link.description}
+                </Link>
+              )}
             </NavItem>
           ))}
         </Nav>
