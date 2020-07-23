0740d954a7c08ca6a82857b922a29624d41613ee
Raquel.Magnani <quel.magnani88@gmail.com>
2020-07-23 12:26:49 -0300

    add heart img to Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Heart from "../../assets/heart-1.png"
 import "./footer.css"
 
 const Footer =()=>{
     return(
     <footer className="footer--design text-center">
-      Desenvolvido com amor por Raquel M. Pinheiro
+      Desenvolvido com <img className="heart--img" src={Heart} alt="imagem de um coração vazado"/> por Raquel M. Pinheiro
     </footer>
     )
 }
